0a842f7efb22873151fc86f56565a373b86d4fa5
felipefaveri <126569718+felipefaveri@users.noreply.github.com>
2023-10-05 11:10:47 -0300

    Update main.js

--- AluraMidi-main/main.js
+++ AluraMidi-main/main.js
@@ -1,3 +1,9 @@
 function TocaSomPom(){
 document.querySelector('.tecla_pom').onclik= tocaSomPom;
 }
+document.querySelector('#som_tecla_pom').play();
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclik= tocaSomClap;
