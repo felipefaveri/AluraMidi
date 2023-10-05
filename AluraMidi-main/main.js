1cd8d5888cac45abd2ff9b72124ec80a1a984a09
felipefaveri <126569718+felipefaveri@users.noreply.github.com>
2023-10-05 11:14:54 -0300

    Update main.js

--- AluraMidi-main/main.js
+++ AluraMidi-main/main.js
@@ -7,3 +7,28 @@ function TocaSomPom(){
 document.querySelector('#Som_tecla_clap').play();
 }
 document.querySelector('.tecla_clap').onclik= tocaSomClap;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclik= tocaSomTim;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclik= tocaSomPuff;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclik= tocaSomSplash;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclik= tocaSomToim;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_pshi').play();
+}
+document.querySelector('.tecla_pshi').onclik= tocaSomPshi;
